f3228055e8d89713b916e9ba5440854b046c97c7
muntasir17 <166358439+muntasir17@users.noreply.github.com>
2025-08-13 11:51:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let scrollRevealOption = {
+    distance: "15px",
+    duration: 400,
+};
+ScrollReveal().reveal(".conta", {
+    ...scrollRevealOption,
+    origin: "top",
+});
+ScrollReveal().reveal(".conta", {
+     ...scrollRevealOption,
+     delay: 400,
+});
+
+let btns = document.querySelectorAll(".btn"); 
+let winnerMessage = document.querySelector(".winnerMessage");
+btns.forEach((key)=>{
+  key.addEventListener("click",()=>{
+let arr = ["Rock","Paper","Scissors"];
+let ComputerChoise = arr[Math.floor(Math.random() * arr.length)];
+console.log(ComputerChoise);
+let yourCHoise = key.dataset.store;
+let winner = "";
+if(ComputerChoise === yourCHoise){
+  winner = "Draw";
+}else if(
+  (yourCHoise === "Rock" && ComputerChoise === "Scissors") || 
+  (yourCHoise === "Scissors" && ComputerChoise === "Paper") || 
+  (yourCHoise === "Paper" && ComputerChoise === "Rock") 
+){
+  winner = "You Win";
+}else{
+    winner = "Computer Win";
+}
+winnerMessage.innerHTML = `You Choose ${yourCHoise}. Computer Choose ${ComputerChoise}. Result ${winner}`
+  });
+});
